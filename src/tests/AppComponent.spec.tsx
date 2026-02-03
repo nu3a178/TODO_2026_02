ef9a8cf591b1dcc35048a308a5d05c857c9c8d0f
nu3a178 <nu197ma@gmail.com>
2026-02-04 07:58:30 +0900

    testを通す

--- src/tests/AppComponent.spec.tsx
+++ src/tests/AppComponent.spec.tsx
@@ -2,9 +2,14 @@
 import { expect, test } from "vitest";
 import { render, screen } from "@testing-library/react";
 import App from "../App";
+import { Provider } from "@/components/ui/provider";
 
 test("title is HelloWorld", async () => {
-  render(<App />);
-  const target = screen.getByRole("heading", { level: 1 });
-  expect(target).toHaveTextContent("Hello World");
+  render(
+    <Provider>
+      <App />
+    </Provider>,
+  );
+  const target = await screen.findByRole("heading", { level: 1 });
+  expect(target).toHaveTextContent("学習記録アプリ");
 });
